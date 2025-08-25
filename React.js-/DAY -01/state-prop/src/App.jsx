7b4d15cac3cb745198c4748899fc9cb31df1f582
Rahul Patil <rahulpatil6451@gmail.com>
2025-08-25 20:43:38 +0530

    Added all Day folders (Day1, Day2, Day3...)

---
+++ React.js-/DAY -01/state-prop/src/App.jsx
@@ -0,0 +1,16 @@
+import './app.css'
+import { useState } from "react";
+import Account from "./Account";
+
+export default function App(){
+  const [name, setname] = useState("Rohit");
+  const [post, setpost] = useState("Front-End Developer");
+  const [pic, setpic] = useState("https://images.pexels.com/photos/2887774/pexels-photo-2887774.jpeg")
+  return(
+    <>
+    <div id='container'>
+      <Account name={name} post={post} pic={pic}/>
+    </div>
+    </>
+  )
+}
