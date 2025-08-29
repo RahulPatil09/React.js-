cfce40c9f46d3d44eadb632954b455b318c7ab5a
Rahul Patil <rahulpatil6451@gmail.com>
2025-08-29 13:24:56 +0530

    1 Fragments commit

---
+++ Fragments/src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+function App() {
+  return (
+
+    <React.Fragment>
+      <h1>Healthy Food</h1>
+      <ul class="list-group">
+        <li class="list-group-item"> Apple</li>
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">rice</li>
+        <li class="list-group-item">roti</li>
+        <li class="list-group-item">green vegitables</li>
+        </ul>
+        </React.Fragment>
+  )
+}
+export default App
